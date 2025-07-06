d03c3a8d5783f06d3b8d2ea400fed32022ef76ac
Kelubeh <kelubeh@gmail.com>
2025-07-06 05:12:42 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p>Hello World </p>
+      <p>Welcome to React</p>
+      <p>Enjoy coding!</p>
+      <p>Have a great day!</p>
+      <p>Keep learning!</p>
+    </div>
+  )
+}
+
+export default App
